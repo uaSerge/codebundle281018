ad65f0832309038131b085a1c9f84c12118f6832
Serhii <uaserge2@gmail.com>
2018-10-29 15:23:50 +0200

    OK commit 29/10/2018 15.20

--- src/aura/merItem/merItemController.js
+++ src/aura/merItem/merItemController.js
@@ -2,16 +2,24 @@
  * Created by Serhii on 24.10.2018.
  */
 ({
+
+    doInit: function (component, event, helper) {
+        var item = component.get("v.item");
+        var quantityReal = component.find("quantityReal");
+        var quantityRealNew = item["Quantity__c"]
+        quantityReal.set("v.value", quantityRealNew);
+    },
+
     toCart: function (component, event, helper) {
         var newQuan = component.find("quantityBuy");
         console.log("v.quantityBuy " + newQuan.get("v.value"));
         var newItem = component.get("v.item");
-        if ((newQuan.get("v.value") >= 1) && (newQuan.get("v.value") <= newItem["Quantity__c"])) {
+        var quantityReal_2 = component.find("quantityReal").get("v.value");
+        if ((newQuan.get("v.value") >= 1) && (newQuan.get("v.value") <= quantityReal_2)) {
             var newQuanValue = newQuan.get("v.value");
             newItem["Quantity__c"] = newQuanValue;
             console.log("Create item: " + JSON.stringify(newItem));
             helper.createItem(component, newItem);
         } else alert("Enter correct amount of products.");
-
     },
 })
